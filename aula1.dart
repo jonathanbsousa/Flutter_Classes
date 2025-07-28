e25b858b3fb15976134e51752677d27e8f8086d4
jonathanbsousa <jonathanbsousa55@gmail.com>
2025-07-28 17:04:32 -0300

    First Classa 28/03/2025

---
+++ aula1.dart
@@ -0,0 +1,118 @@
+import 'dart:io';
+import 'dart:mirrors'; //importação das bibliotecas dart
+
+void main() {
+  //   int op = 0;
+
+  //   do {
+  //     print(
+  //       "Digite a opção desejada: \n1 - Somar \n2 - Subtrair \n3 - Multiplicar \n4 - Dividir \n5 - Sair",
+  //     );
+  //     String? op_input = stdin.readLineSync();
+  //     op = int.parse(op_input!);
+
+  //     switch (op) {
+  //       case 1:
+  //         print("Digite um numero: ");
+  //         String? input1 = stdin.readLineSync();
+  //         int num1 = int.parse(input1!);
+
+  //         print("Digite outro numero: ");
+  //         String? input2 = stdin.readLineSync();
+  //         int num2 = int.parse(input2!);
+
+  //         print(num1 + num2);
+  //         break;
+
+  //       case 2:
+  //         print("Digite um numero: ");
+  //         String? input1 = stdin.readLineSync();
+  //         int num1 = int.parse(input1!);
+
+  //         print("Digite outro numero: ");
+  //         String? input2 = stdin.readLineSync();
+  //         int num2 = int.parse(input2!);
+
+  //         print(num1 - num2);
+  //         break;
+
+  //       case 3:
+  //         print("Digite um numero: ");
+  //         String? input1 = stdin.readLineSync();
+  //         int num1 = int.parse(input1!);
+
+  //         print("Digite outro numero: ");
+  //         String? input2 = stdin.readLineSync();
+  //         int num2 = int.parse(input2!);
+
+  //         print(num1 * num2);
+  //         break;
+
+  //       case 4:
+  //         print("Digite um numero: ");
+  //         String? input1 = stdin.readLineSync();
+  //         int num1 = int.parse(input1!);
+
+  //         print("Digite outro numero: ");
+  //         String? input2 = stdin.readLineSync();
+  //         int num2 = int.parse(input2!);
+
+  //         if (num1 == 0 || num2 == 0) {
+  //           print("Não é possível dividir por 0");
+  //         } else {
+  //           print(num1 / num2);
+  //         }
+  //         break;
+
+  //       case 5:
+  //         print("Saindo");
+  //         break;
+
+  //       default:
+  //         print("Opção Invalida");
+  //     }
+  //   } while (op < 5);
+
+  //   // ----------------------------------------------------------
+
+  //   print("Digite sua idade");
+  //   String? entrada = stdin.readLineSync();
+  //   int idade = int.parse(entrada!);
+
+  //   if (idade >= 18) {
+  //     print("Você pode dirigir!");
+  //   } else if (idade == 1) {
+  //     print("Nem pense em dirigir");
+  //   } else {
+  //     print("Você não pode dirigir!");
+  //   }
+
+  // print("Digite sua idade: ");
+  // String? entrada = stdin.readLineSync();
+  // int idade = int.parse(entrada!);
+  // print("Ano que vem você terá " + (idade + 1).toString() + " anos");
+
+  // // ----------------------------------------------------------
+
+  double somaNotas = 0;
+
+  for (int i = 0; i < 5; i++) {
+    print("Digite a nota do aluno:");
+    String nota = stdin.readLineSync()!;
+    double notaAluno = double.parse(nota);
+    somaNotas += notaAluno;
+  }
+
+  double media = somaNotas / 5;
+
+  print("Média final: $media");
+
+  if (media >= 5) {
+    print("Aprovado");
+  } else if (media > 4) {
+    print("Recuperação");
+  } else {
+    print("Reprovado");
+  }
+  
+}
